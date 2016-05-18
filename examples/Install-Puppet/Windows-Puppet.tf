2815faa8ddc32b5ca0175138444b17c3a4be9f5f
Andrew Carter <a.p.carter@usa.net>
2016-05-18 14:40:51 +0100

    Update Windows-Puppet.tf

--- examples/Install-Puppet/Windows-Puppet.tf
+++ examples/Install-Puppet/Windows-Puppet.tf
@@ -1,14 +1,20 @@
+provider "aws" {
+    access_key = "${var.access_key}"
+    secret_key = "${var.secret_key}"
+    region = "${var.region}"
+}
 
 resource "aws_instance" "win-pup" {
     ami                         = "ami-9ebb39ed"
+    region                      = "${var.region}"
     instance_type               = "t2.micro"
-    key_name                    = "<YOUR KEY PAIR NAME>"
-    security_groups             = ["<YOUR SECURITY GROUP ID>"]
+    key_name                    = "${var.keypair_name}"
+    security_groups             = ["${var.security_group_id}"]
 
     user_data = <<EOF
     <powershell>
         $MsiUrl = "https://downloads.puppetlabs.com/windows/puppet-agent-x64-latest.msi"
-        $install_args = @("/qn","/norestart","/i",$MsiUrl,"PUPPET_MASTER_SERVER=<YOUR SERVER PATH>")
+        $install_args = @("/qn","/norestart","/i",$MsiUrl,"PUPPET_MASTER_SERVER=${var.puppet_master}")
         Start-Process -FilePath msiexec.exe -ArgumentList $install_args -Wait -PassThru
     </powershell>
     EOF
