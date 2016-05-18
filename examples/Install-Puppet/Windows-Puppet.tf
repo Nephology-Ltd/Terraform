0b03c3667409524439c3affb5e19a04a0156e05c
Andrew Carter <a.p.carter@usa.net>
2016-05-18 15:37:18 +0100

    Update Windows-Puppet.tf

--- examples/Install-Puppet/Windows-Puppet.tf
+++ examples/Install-Puppet/Windows-Puppet.tf
@@ -4,12 +4,47 @@ provider "aws" {
     region = "${var.region}"
 }
 
+resource "aws_security_group" "allow_puppet_RDP" {
+  name = "allow_puppet_RDP"
+  description = "Allow Puppet and RDP"
+
+  ingress {
+      from_port = 443
+      to_port = 443
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+
+ ingress {
+      from_port = 8140 
+      to_port = 8140
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+   ingress {
+      from_port = 61613
+      to_port = 61613
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+   ingress {
+      from_port = 3389
+      to_port = 3389
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+ 
+}
+
+
 resource "aws_instance" "win-pup" {
     ami                         = "ami-9ebb39ed"
     region                      = "${var.region}"
     instance_type               = "t2.micro"
     key_name                    = "${var.keypair_name}"
-    security_groups             = ["${var.security_group_id}"]
+    security_groups             = ["aws_security_group.allow_puppet_RDP.id"]
 
     user_data = <<EOF
     <powershell>
