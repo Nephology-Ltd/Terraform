43b58838f50ad8d92626e223ab2a40a711016650
Andrew Carter <a.p.carter@usa.net>
2016-05-23 11:00:53 +0100

    Update Windows-Choco.tf

--- examples/Install-Chocolatey/Windows-Choco.tf
+++ examples/Install-Chocolatey/Windows-Choco.tf
@@ -3,3 +3,19 @@ provider "aws" {
     secret_key = "${var.secret_key}"
     region = "${var.region}"
 }
+
+resource "aws_instance" "win-choc" {
+    ami                         = "ami-9ebb39ed"
+    region                      = "${var.region}"
+    instance_type               = "t2.micro"
+    key_name                    = "${var.keypair_name}"
+    
+    user_data = <<EOF
+    <powershell>
+
+    </powershell>
+    EOF
+    tags {
+        Name = "Windows-Chocolatey"
+    }
+}
